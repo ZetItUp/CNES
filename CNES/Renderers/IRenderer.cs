539b767b0efcaa0244c3fd199ed7c41eb744e3bc
Andreas Lindström <zetitup@gmail.com>
2025-06-07 00:11:35 +0200

    Add project files.

---
+++ CNES/Renderers/IRenderer.cs
@@ -0,0 +1,11 @@
+﻿using System.Drawing;
+
+namespace CNES.Renderers
+{
+    public interface IRenderer
+    {
+        void Initialize(int width, int height);
+        void RenderFrame(Color[] framebuffer);
+        void Clear(Color clearColor);
+    }
+}
